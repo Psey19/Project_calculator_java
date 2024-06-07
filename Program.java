b98fca1e7fa7c71bf0855f4d81762b12858f7103
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-06-07 14:22:58 +0500

    Create Program.java

---
+++ Program.java
@@ -0,0 +1,14 @@
+package HomeWork_7;
+
+import HomeWork_7.Controller.Controller;
+import HomeWork_7.View.View;
+
+public class Program {
+    public static void main(String[] args) {
+        Controller controller = new Controller(new View()); {
+        }
+        controller.buttonClick();
+
+
+    }
+}
