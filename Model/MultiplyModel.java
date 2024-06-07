5156fc9f7b33b6cf7919fa40162778305aa24be1
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-06-07 14:18:21 +0500

    Create MultiplyModel.java

---
+++ Model/MultiplyModel.java
@@ -0,0 +1,21 @@
+package HomeWork_7.Model;
+
+public class MultiplyModel extends CalcModel {
+    public MultiplyModel() {
+    }
+
+    @Override
+    public double result() {
+        return x * y;
+    }
+
+    @Override
+    public void setX(double value) {
+        super.x = value;
+    }
+
+    @Override
+    public void setY(double value) {
+        super.y = value;
+    }
+}
