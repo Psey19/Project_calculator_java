ae48377b963b3c43b249d5833db4600d39c6dbd1
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-06-07 14:22:25 +0500

    Create View.java

---
+++ View/View.java
@@ -0,0 +1,16 @@
+package HomeWork_7.View;
+
+import java.util.Scanner;
+
+public class View {
+    Scanner scanner = new Scanner(System.in);
+
+    public double getValue(String nameValue){
+        System.out.printf("%s", nameValue);
+        return scanner.nextDouble();
+    }
+
+    public void printResult(double result, String title){
+        System.out.printf("%s %f\n", title, result);
+    }
+}
