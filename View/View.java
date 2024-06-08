54501131907bab2f4e1e06178595cbeeb6cf4c64
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-06-08 19:22:26 +0500

    Update View.java

--- View/View.java
+++ View/View.java
@@ -1,16 +1,16 @@
 package HomeWork_7.View;
 
-import java.util.Scanner;
+import HomeWork_7.Log.Log;
+import HomeWork_7.Model.ModelGen;
 
-public class View {
-    Scanner scanner = new Scanner(System.in);
-
-    public double getValue(String nameValue){
-        System.out.printf("%s", nameValue);
-        return scanner.nextDouble();
-    }
-
-    public void printResult(double result, String title){
-        System.out.printf("%s %f\n", title, result);
-    }
+public interface View {
+    //Метод по выбору Логгера, который соответствует арифметическим операциям для View
+    Log startLog();
+    //Метод по выбору Генератора Модели, которая соответствует арифметическим операциям для View
+    ModelGen getModelGen();
+    int getChoice();
+    double getValue(String nameValue);
+    void printResult(double result);
+    int choiceOperation();
+    void getOperationName(int choice);
 }
