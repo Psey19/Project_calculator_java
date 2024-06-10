cb07a142a86fe56cb8d873e4e119e9f08b407c53
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-06-10 17:52:48 +0500

    Update Program.java

--- Program.java
+++ Program.java
@@ -2,11 +2,12 @@ package HomeWork_7;
 
 import HomeWork_7.Controller.Controller;
 
+import HomeWork_7.Controller.ControllerSumMultyDiv;
 import HomeWork_7.View.ViewSumMultyDiv;
 
 public class Program {
     public static void main(String[] args) {
-        Controller controller = new Controller(new ViewSumMultyDiv());
+        Controller controller = new ControllerSumMultyDiv(new ViewSumMultyDiv());
         controller.buttonClick();
     }
 }
