1d2bec99489997e68b3c76fec2380881e3518470
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-06-10 17:38:00 +0500

    Update View.java

--- View/View.java
+++ View/View.java
@@ -1,13 +1,8 @@
 package HomeWork_7.View;
 
-import HomeWork_7.Log.Log;
-import HomeWork_7.Model.ModelGen;
+
 
 public interface View {
-    //Метод по выбору Логгера, который соответствует арифметическим операциям для View
-    Log startLog();
-    //Метод по выбору Генератора Модели, которая соответствует арифметическим операциям для View
-    ModelGen getModelGen();
     int getChoice();
     double getValue(String nameValue);
     void printResult(double result);
