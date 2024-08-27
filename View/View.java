59129de5e5a3dc492928daee58d221767692fa91
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-08-27 23:01:14 +0500

    Update View.java

--- View/View.java
+++ View/View.java
@@ -3,9 +3,11 @@ package HomeWork_7.View;
 
 
 public interface View {
-    int getChoice();
-    double getValue(String nameValue);
+    String getChoice();
+    String getValue(String nameValue);
     void printResult(double result);
-    int choiceOperation();
-    void getOperationName(int choice);
+    String choiceOperation();
+    void getOperationName(String choice);
+    Double enterFirstNumber();
+    Double enterSecondNumber();
 }
