b58a38cb333f19772c0b8024e6c3f5977125383e
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-08-27 23:03:08 +0500

    Update ModelGen.java

--- Model/ModelGen.java
+++ Model/ModelGen.java
@@ -2,5 +2,5 @@ package HomeWork_7.Model;
 
 //Здесь применен паттер Фарбричный метод для создания Модели арифметической операции по выбранному номеру в списке
 public interface ModelGen {
-    Model createModel(int operation);
+    Model createModel(String operation);
 }
