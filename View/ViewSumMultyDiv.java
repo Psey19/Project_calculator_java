69b593f0e5470dd79e03e42e1429f1c298b648a0
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-08-27 23:02:04 +0500

    Update ViewSumMultyDiv.java

--- View/ViewSumMultyDiv.java
+++ View/ViewSumMultyDiv.java
@@ -4,46 +4,75 @@ package HomeWork_7.View;
 import java.util.Scanner;
 
 public class ViewSumMultyDiv implements View {
-    Scanner scanner = new Scanner(System.in);
-    private int choice;
-    
+    Scanner sc = new Scanner(System.in);
+    private String choice;
+    private String a;
+    private String b;
+
     @Override
-    public int getChoice() {
+    public String getChoice() {
         return this.choice;
     }
-    
     @Override
-    public double getValue(String nameValue){
+    public String getValue(String nameValue){
         System.out.printf("%s", nameValue);
-        return scanner.nextDouble();
+        return sc.nextLine();
     }
-    
     @Override
     public void printResult(double result){
         System.out.printf("Результат: %f\n", result);
     }
-    
     @Override
-    public int choiceOperation(){
+    public String choiceOperation(){
         System.out.printf("\nВыберите арифметическую операцию: \n");
         System.out.println("1 - '+' ");
         System.out.println("2 - '*' ");
         System.out.println("3 - '/' ");
-        return scanner.nextInt();
+        return sc.nextLine();
     }
-    
     @Override
-    public void getOperationName(int choice){
+    public void getOperationName(String choice){
         this.choice = choice;
-        if (choice == 1)
+        boolean isInteger = choice.matches("-?\\d+");
+        if (!isInteger) {
+            System.out.println("Вы ввели не число, повторите ввод операции");
+            getOperationName(choiceOperation());
+        }
+        else if (Integer.parseInt(choice) == 1)
             System.out.println("Выбранная операция: сложение");
-        else if (choice == 2)
+        else if (Integer.parseInt(choice) == 2)
             System.out.println("Выбранная операция: умножение");
-        else if (choice == 3)
+        else if (Integer.parseInt(choice) == 3)
             System.out.println("Выбранная операция: деление");
         else {
             System.out.println("Такой операции в списке нет, повторите ввод операции");
-            this.choice = choiceOperation();
+            getOperationName(choiceOperation());
         }
+    }
+
+    @Override
+    public Double enterFirstNumber() {
+        System.out.println("Задайте 1-е вещественное число");
+        String a = getValue("a: ");
+        this.a = a;
+        boolean isInteger = a.matches("[-+]?\\d*\\.?\\d+([eE][-+]?\\d+)?");
+        if (!isInteger) {
+            System.out.println("Вы ввели не вещественное число, повторите ввод операции");
+            enterFirstNumber();
+        }
+        return Double.parseDouble(this.a);
+    }
+
+    @Override
+    public Double enterSecondNumber() {
+        System.out.println("Задайте 2-е вещественное число");
+        String b = getValue("b: ");
+        this.b = b;
+        boolean isInteger = b.matches("[-+]?\\d*\\.?\\d+([eE][-+]?\\d+)?");
+        if (!isInteger) {
+            System.out.println("Вы ввели не вещественное число, повторите ввод операции");
+            enterSecondNumber();
+        }
+        return Double.parseDouble(this.b);
     }
 }
